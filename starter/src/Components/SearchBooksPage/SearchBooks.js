88dc5444c6f7e4ec156f7ac4eccdc96efaf981cf
Juan Aguilera <jefraix@gmail.com>
2023-08-01 01:23:55 -0400

    Search page fetches books and books can be added to bookshelf, but still buggy

--- starter/src/Components/SearchBooksPage/SearchBooks.js
+++ starter/src/Components/SearchBooksPage/SearchBooks.js
@@ -1,6 +1,37 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
-const SearchBooks = ({ shelfs }) => {
+import Book from "../Book";
+
+import * as BooksAPI from "../../BooksAPI"
+
+const SearchBooks = ({ bookidMappings, OnBookshelfChange }) => {
+  const [query, SetQuery] = useState("")
+  const [bookResults, SetBookResults] = useState([])
+
+  const updateResults = (query) => {
+    const performSearch = async () => {
+      const res = await BooksAPI.search(query, 20)
+      console.log(res)
+
+      if (Array.isArray(res)) {
+        const resBooks = res.map(book => {
+          return {
+            id: book.id,
+            title: book.title,
+            authors: book.authors,
+            shelf: "none",
+            imageURL: book.imageLinks.thumbnail
+          }
+        })
+        SetBookResults(resBooks)
+      }
+    }
+
+    if (query !== "") performSearch()
+    SetQuery(query.trim())
+  }
+
   return (
     <div className="search-books">
       <div className="search-books-bar">
@@ -9,11 +40,22 @@ const SearchBooks = ({ shelfs }) => {
           <input
             type="text"
             placeholder="Search by title, author, or ISBN"
+            value={query}
+            onChange={(e) => updateResults(e.target.value)}
           />
         </div>
       </div>
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+          {bookResults.map((book, idx) =>
+            <li key={idx}>
+              <Book 
+                Book={book}
+                OnBookshelfChange={OnBookshelfChange}
+              />
+            </li>
+          )}
+        </ol>
       </div>
     </div>
   )
